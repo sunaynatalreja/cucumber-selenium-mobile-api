feb7a65b1e64c7b7bad622fc9baacc15b3fcd3ae
sunaynatalreja <52531057+sunaynatalreja@users.noreply.github.com>
2021-06-16 13:36:33 +0530

    pushed mobile automation demo changes

---
+++ src/main/java/prjframework/sunaynatalreja/framework/pages/MobileAutomationScript.java
@@ -0,0 +1,179 @@
+/**
+ * 
+ */
+package prjframework.sunaynatalreja.framework.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import prj.sunaynatalreja.webdriverutil.browserutil.BrowserElementActions;
+import prj.sunaynatalreja.webdriverutil.mobileutil.MobileElementActions;
+
+/**
+ * @author Sunayna Talreja
+*/
+public class MobileAutomationScript {
+	
+	private boolean isPageLoaded;
+	WebDriver driver=null;
+	
+	@AndroidFindBy(uiAutomator   = "text(\"Preference\")")
+	private AndroidElement preferences;
+	
+	@AndroidFindBy(xpath  = "//android.widget.TextView[@text='3. Preference dependencies']")
+	private AndroidElement preferenceDependency;
+	
+	@AndroidFindBy(className  = "android.widget.CheckBox")
+	private AndroidElement wifiCheckBox;
+	
+	
+	@AndroidFindBy(className  = "android.widget.EditText")
+	private AndroidElement wifiTextBox;
+	
+	@AndroidFindBy(xpath  = "//android.widget.TextView[@text='WiFi settings']")
+	private AndroidElement wifiSettings;
+	
+	@AndroidFindBy(id="android:id/button1")
+	private AndroidElement wifiOkButton;
+	
+	@AndroidFindBy(uiAutomator ="clickable(\"true\")")
+	private List<AndroidElement> clickableCheck;
+	
+	@AndroidFindBy(uiAutomator ="text(\"Views\")")
+	private AndroidElement views;
+	
+	@AndroidFindBy(uiAutomator ="text(\"Expandable Lists\")")
+	private AndroidElement expandableList;
+	
+	@AndroidFindBy(uiAutomator ="text(\"1. Custom Adapter\")")
+	private AndroidElement customAdaptor;
+	
+	@AndroidFindBy(uiAutomator ="text(\"People Names\")")
+	private AndroidElement peopleNames;
+	
+	@AndroidFindBy(uiAutomator ="text(\"Date Widgets\")")
+	private AndroidElement dateWidgets;
+	
+	@AndroidFindBy(uiAutomator ="text(\"2. Inline\")")
+	private AndroidElement inline;
+	
+	@AndroidFindBy(xpath  = "//*[@content-desc='9']")
+	private AndroidElement inlineDate;
+	
+	@AndroidFindBy(xpath  = "//*[@content-desc='15']")
+	private AndroidElement startTime;
+	@AndroidFindBy(xpath  = "//*[@content-desc='45']")
+	private AndroidElement endTime;
+	
+	@AndroidFindBy(uiAutomator ="new UiScrollable(new UiSelector()).scrollIntoView(text(\"Lists\"));")
+	private AndroidElement lists;
+	
+	public MobileAutomationScript(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+
+	}
+
+	
+	
+	public void clickPreferences() throws Exception
+	{
+		
+		  BrowserElementActions.getInstance().clickOnElement(driver, preferences,
+		  "Preference clicked");		 
+		
+		
+	}
+	
+	public void clickPreferenceDependency() throws Exception
+	{
+		
+		  
+		clickClickable(); 
+		
+		BrowserElementActions.getInstance().clickOnElement(driver, preferenceDependency,
+		  "Preference clicked");		 
+		
+	}
+	
+	public void clickWifiCheckBox() throws Exception
+	{
+		
+		BrowserElementActions.getInstance().clickOnElement(driver, wifiCheckBox,
+		  "Wifi Checkbox clicked");		 
+		//preferences.click();
+		
+	}
+	public void enterWifiDetails() throws Exception
+	{
+		
+		BrowserElementActions.getInstance().typeOnElement(driver,wifiTextBox,"Text", "Wifi text box details entered");		 
+		BrowserElementActions.getInstance().clickOnElement(driver, wifiOkButton, "Clicked OK");
+		
+	}
+	
+	public void clickWifiSettings() throws Exception
+	{
+		
+		  
+		
+		BrowserElementActions.getInstance().clickOnElement(driver, wifiSettings,"Wifi text box details entered");		 
+	
+		
+	}
+
+	
+	public void clickClickable() throws Exception
+	{
+		
+		 clickableCheck.size();	 
+		
+	}
+	
+	public void clickViews() throws Exception
+	{
+		MobileElementActions.getInstance().mobileTap(driver, views,"Clicked Views");	
+	}
+	public void clickExpandableList() throws Exception
+	{
+		MobileElementActions.getInstance().mobileTap(driver, expandableList,"Clicked Views");	
+	}
+	public void clickcustomAdapter() throws Exception
+	{
+		MobileElementActions.getInstance().mobileTap(driver, customAdaptor,"Clicked Views");	
+	}
+	public void longPressPeopleNames() throws Exception
+	{
+		MobileElementActions.getInstance().longPressMobileElement(driver, peopleNames,"Clicked Views");	
+	}
+	public void clickDateWidgets() throws Exception
+	{
+		MobileElementActions.getInstance().mobileTap(driver, dateWidgets,"Clicked Views");	
+	}
+	public void clickInline() throws Exception
+	{
+		MobileElementActions.getInstance().mobileTap(driver, inline,"Clicked Views");	
+	}
+	
+	public void clickDateInline() throws Exception
+	{
+		MobileElementActions.getInstance().mobileTap(driver, inlineDate,"Clicked Views");	
+	}
+	
+	public void clickNewTime() throws Exception
+	{
+		MobileElementActions.getInstance().mobileSlider(driver, endTime, startTime, "start end time");	
+	}
+	
+	public void scrollToList() throws Exception
+	{		
+		MobileElementActions.getInstance().mobileTap(driver, lists, "start end time");	
+	}
+	
+}
---
+++ src/test/java/prjframework/sunaynatalreja/framework/testscripts/mobile/MobileTestAutomation.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package prjframework.sunaynatalreja.framework.testscripts.mobile;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import prj.sunaynatalreja.webdriverutil.driverutil.WebDriverFactory;
+import prjframework.sunaynatalreja.framework.listeners.ExtentReportListener;
+import prjframework.sunaynatalreja.framework.pages.MobileAutomationScript;
+
+
+@Listeners(ExtentReportListener.class)
+
+/**
+ * @author Sunayna Talreja
+ */
+public class MobileTestAutomation {
+
+	WebDriver driver;
+	MobileAutomationScript mobileAutomationScript;
+	WebDriverFactory webDriverFactory;
+
+	@BeforeClass
+	public void beforeClass()
+	{
+		webDriverFactory = new WebDriverFactory();
+	}
+
+	@Test(enabled = false)
+	public void testMobile(ITestContext context) throws Exception
+	{
+		String currentDirectory = System.getProperty("user.dir");
+		System.out.println(currentDirectory);
+
+		driver=webDriverFactory.get(context.getCurrentXmlTest().getParameter("browser"),"http://127.0.0.1:4723/wd/hub","Android Device",currentDirectory+ "\\GridSetup\\ApiDemos-debug.apk");
+		mobileAutomationScript=new MobileAutomationScript(driver);
+
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		mobileAutomationScript.clickPreferences();
+		mobileAutomationScript.clickPreferenceDependency();
+		mobileAutomationScript.clickWifiCheckBox();
+		mobileAutomationScript.clickWifiSettings();
+		mobileAutomationScript.enterWifiDetails();
+	}
+
+	@Test(enabled = false)
+	public void testMobileGestures(ITestContext context) throws Exception
+	{
+		String currentDirectory = System.getProperty("user.dir");
+		System.out.println(currentDirectory);
+		driver=webDriverFactory.get(context.getCurrentXmlTest().getParameter("browser"),"http://127.0.0.1:4723/wd/hub","Android Device",currentDirectory+ "\\GridSetup\\ApiDemos-debug.apk");
+		mobileAutomationScript=new MobileAutomationScript(driver);
+
+		mobileAutomationScript.clickViews();
+		mobileAutomationScript.clickExpandableList();
+		mobileAutomationScript.clickcustomAdapter();
+		mobileAutomationScript.longPressPeopleNames();
+		//mobileAutomationScript.enterWifiDetails();
+	}
+	@Test(enabled = false)
+	public void testMobileSwipe(ITestContext context) throws Exception
+	{
+		String currentDirectory = System.getProperty("user.dir");
+		System.out.println(currentDirectory);
+
+		driver=webDriverFactory.get(context.getCurrentXmlTest().getParameter("browser"),"http://127.0.0.1:4723/wd/hub","Android Device",currentDirectory+ "\\GridSetup\\ApiDemos-debug.apk");
+		mobileAutomationScript=new MobileAutomationScript(driver);
+
+		mobileAutomationScript.clickViews();
+		mobileAutomationScript.clickDateWidgets();
+		mobileAutomationScript.clickInline();
+		mobileAutomationScript.clickDateInline();
+		mobileAutomationScript.clickNewTime();
+	}
+
+	@Test(enabled = true)
+	public void testMobileScroll(ITestContext context) throws Exception
+	{
+		String currentDirectory = System.getProperty("user.dir");
+		System.out.println(currentDirectory);
+
+		driver=webDriverFactory.get(context.getCurrentXmlTest().getParameter("browser"),"http://127.0.0.1:4723/wd/hub","Android Device",currentDirectory+ "\\GridSetup\\ApiDemos-debug.apk");
+		mobileAutomationScript=new MobileAutomationScript(driver);
+		mobileAutomationScript.clickViews();
+		mobileAutomationScript.scrollToList();
+
+	}
+
+}
